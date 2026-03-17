b4a32bb27d39fffc0fb32799ab2cefad88316c0e
Sravya Sirapu <sravyasirapu.1924@gmail.com>
2026-03-17 15:52:29 +0530

    Fixed port for deployment

--- main.py
+++ main.py
@@ -109,6 +109,13 @@ async def analyze_sector(sector: str, request: Request):
     except Exception as e:
         return {"error": "Internal Error", "details": str(e)}
 
+# if __name__ == "__main__":
+#     import uvicorn
+#     uvicorn.run(app, host="127.0.0.1", port=8000)
+
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    import os
+    # This line tells the code to use the Port the server provides
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
